82672d9645451be8886a4db636d2302bc8307cdb
EmranAli-15 <mohammademranali15@gmail.com>
2026-05-09 14:23:46 +0600

    rag is working properly

--- src/app/modules/book/book.service.ts
+++ src/app/modules/book/book.service.ts
@@ -36,6 +36,17 @@ const getBooksByWriter = async ({ id }: { id: string}) => {
 const getAllBooks = async ({ pageNum, limitNum }: { pageNum: number, limitNum: number }) => {
     const limit = limitNum || 8;
     const result = await Book.find().skip(pageNum * limit).limit(limit).populate("category", "name").populate("writer", "name");
+    // const result:any = await Book.find().populate("category", "name").populate("writer", "name");
+    // console.log(result)
+    // for(let i=0; i<result.length; i++){
+    //     // console.log(`${i+1}) ${result[i]?.name} - (মূল্যঃ ${result[i]?.price} টাকা)`)
+        
+    //     const text1 = `আমাদের কাছে ${result[i]?.writer?.name}-এর লেখা ${result[i]?.name} বইটি আছে, ${result[i]?.name} বইটির মূল্য বা দাম (মূল্যঃ ${result[i]?.price} টাকা)। We have the *${result[i]?.name}* book by-*${result[i]?.writer?.name}* writer, price of *${result[i]?.name}* book is ${result[i]?.price}`
+
+    //     const text2 = ` ${result[i]?.writer?.name}-এর লেখা ${result[i]?.name} বইটি পাওয়া যাবে। ${result[i]?.name} বইটির দাম (মূল্যঃ ${result[i]?.price} টাকা)। *${result[i]?.name}* by *${result[i]?.writer?.name}* book is in our stock, price of *${result[i]?.name}* book is ${result[i]?.price}`
+    //     console.log(text1 + text2)
+    // }
+
     return result;
 };
 
