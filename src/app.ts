f1bc7efd6e40ab3f8a6a1ab5d6f5f6ed15e56fdd
EmranAli-15 <mohammademranali15@gmail.com>
2026-05-11 16:08:14 +0600

    all works properly

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ export const port = process.env.PORT || 5000;
 app.use(cors({
   origin: ['https://bookworld-client.vercel.app', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  // allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
